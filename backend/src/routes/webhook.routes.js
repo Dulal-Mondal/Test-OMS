32a5c762b473cbe89b55139cb038b49c0e015adb
Dulal-Mondal <dm640183@gmail.com>
2026-06-20 23:45:38 +0600

    fix: webhook

--- backend/src/routes/webhook.routes.js
+++ backend/src/routes/webhook.routes.js
@@ -1,27 +1,17 @@
-// const router = require('express').Router();
-// const ctrl = require('../controllers/webhook.controller');
-// const apiKey = require('../middlewares/apiKey.middleware');
-
-// // SoftBrainChat connection test
-// router.get('/ping', ctrl.ping);
-
-// // SoftBrainChat order push (protected by API key)
-// router.post('/order', apiKey, ctrl.receiveOrder);
-
-// module.exports = router;
-
-
-
-
-
-
 const router = require('express').Router();
 const ctrl = require('../controllers/webhook.controller');
+const apiKey = require('../middlewares/apiKey.middleware');
 
 // SoftBrainChat connection test
 router.get('/ping', ctrl.ping);
 
-// SoftBrainChat order push
-router.post('/order', ctrl.receiveOrder);
+// SoftBrainChat order push (protected by API key)
+router.post('/order', apiKey, ctrl.receiveOrder);
 
 module.exports = router;
+
+
+
+
+
+
